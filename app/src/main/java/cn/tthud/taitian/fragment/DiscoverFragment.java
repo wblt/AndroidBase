ff675b7f26eb1690f31b1cc14df06834f38c0f32
Scott_Mr <a632845514@vip.qq.com>
2017-11-01 23:00:27 +0800

    活动页面添加导航栏按钮

--- app/src/main/java/cn/tthud/taitian/fragment/DiscoverFragment.java
+++ app/src/main/java/cn/tthud/taitian/fragment/DiscoverFragment.java
@@ -1,5 +1,6 @@
 package cn.tthud.taitian.fragment;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.view.LayoutInflater;
@@ -7,21 +8,27 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageButton;
 
+import org.xutils.view.annotation.ViewInject;
+
+import java.util.ArrayList;
+
 import cn.tthud.taitian.R;
 import cn.tthud.taitian.base.FragmentBase;
+import cn.tthud.taitian.utils.Log;
 
 /**
  * Created by wb on 2017/10/8.
  */
 
-public class DiscoverFragment extends FragmentBase {
+public class DiscoverFragment extends FragmentBase implements View.OnClickListener {
 
     private View view;
+    private ImageButton top_right_button;
+    private ImageButton top_left_button;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
     }
 
     @Override
@@ -30,9 +37,38 @@ public class DiscoverFragment extends FragmentBase {
             view = super.onCreateView(inflater,container,savedInstanceState);
             appendMainBody(this, R.layout.discover_fragment);
             appendTopBody(R.layout.activity_top_icon);
-            ((ImageButton) view.findViewById(R.id.top_left)).setVisibility(View.INVISIBLE);
-            setTopBarTitle("发现");
+            setTopBarTitle("活动列表");
+
+            initNaviBarView();
+            initView();
         }
         return view;
+    }
+
+    public void initNaviBarView(){
+        top_right_button = (ImageButton) view.findViewById(R.id.top_right_icon);
+        top_right_button.setImageResource(R.drawable.search);
+        top_right_button.setOnClickListener(this);
+
+        top_left_button = (ImageButton) view.findViewById(R.id.top_left);
+        top_left_button.setImageResource(R.drawable.mine_setting);
+        top_left_button.setOnClickListener(this);
+    }
+
+    public void initView(){
+
+    }
+
+    @Override
+    public void onClick(View view) {
+        int id = view.getId();
+        switch (id){
+            case R.id.top_right_icon:
+                Log.i("点击了右侧按钮");
+                break;
+            case R.id.top_left:
+                Log.i("点击左侧按钮");
+                break;
+        }
     }
 }
