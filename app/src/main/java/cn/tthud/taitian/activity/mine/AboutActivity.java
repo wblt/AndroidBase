8a4d5171240718534f2bcc15062e2e1afae6155c
wblt <940422068@qq.com>
2017-11-30 12:50:28 +0800

    d

--- app/src/main/java/cn/tthud/taitian/activity/mine/AboutActivity.java
+++ app/src/main/java/cn/tthud/taitian/activity/mine/AboutActivity.java
@@ -2,11 +2,22 @@ package cn.tthud.taitian.activity.mine;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
+import org.xutils.view.annotation.ViewInject;
 
 import cn.tthud.taitian.R;
 import cn.tthud.taitian.base.ActivityBase;
+import cn.tthud.taitian.utils.VersionUtil;
 
 public class AboutActivity extends ActivityBase {
+
+    @ViewInject(R.id.tv_version)
+    private TextView tv_version;
+
+    @ViewInject(R.id.tv_about)
+    private TextView tv_about;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -15,5 +26,9 @@ public class AboutActivity extends ActivityBase {
         appendTopBody(R.layout.activity_top_text);
         setTopBarTitle("关于我们");
         setTopLeftDefultListener();
+
+        tv_version.setText(VersionUtil.getAppVersionName(AboutActivity.this));
     }
+
+
 }
