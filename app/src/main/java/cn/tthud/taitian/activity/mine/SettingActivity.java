e5a25d6032a11d268b6dd028662be481bb332742
wblt <940422068@qq.com>
2017-11-30 15:05:44 +0800

    d

--- app/src/main/java/cn/tthud/taitian/activity/mine/SettingActivity.java
+++ app/src/main/java/cn/tthud/taitian/activity/mine/SettingActivity.java
@@ -1,7 +1,11 @@
 package cn.tthud.taitian.activity.mine;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+
+import org.xutils.view.annotation.Event;
 
 import cn.tthud.taitian.R;
 import cn.tthud.taitian.base.ActivityBase;
@@ -16,5 +20,28 @@ public class SettingActivity extends ActivityBase {
         setTopBarTitle("设置");
         setTopLeftDefultListener();
 
+    }
+
+    @Event(value = {R.id.edit_phone,R.id.edit_pwd,R.id.lay_bind,R.id.lay_remove},type = View.OnClickListener.class)
+    private void onEvenOnclick(View view) {
+        int id = view.getId();
+        Intent intent;
+        switch (id) {
+            case R.id.edit_phone:
+                intent = new Intent(this,ModifyPhoneActivity.class);
+                startActivity(intent);
+                break;
+            case R.id.edit_pwd:
+                intent = new Intent(this,ModifyPwdActivity.class);
+                startActivity(intent);
+                break;
+            case R.id.lay_bind:
+
+                break;
+            case R.id.lay_remove:
+
+                break;
+
+        }
     }
 }
