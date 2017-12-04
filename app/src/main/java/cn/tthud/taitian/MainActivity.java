d0824857e00b5f1aa907f7a529d3de5b0325d51b
Scott_Mr <a632845514@vip.qq.com>
2017-12-04 17:37:27 +0800

    去掉活动界面上 舞蹈 文字

--- app/src/main/java/cn/tthud/taitian/MainActivity.java
+++ app/src/main/java/cn/tthud/taitian/MainActivity.java
@@ -54,6 +54,8 @@ public class MainActivity extends BaseActivity {
                 .show(homeFragment)
                 .commit();
         DemoApplication.getInstance().setMainActivity(this);
+
+
     }
 
     /**
