6c31a24313662ea8550b328012b3abc6aeec4912
Scott_Mr <a632845514@vip.qq.com>
2017-12-27 13:34:00 +0800

    添加微信支付sdk

---
+++ app/src/main/java/cn/tthud/taitian/utils/Constants.java
@@ -0,0 +1,9 @@
+package cn.tthud.taitian.utils;
+
+/**
+ * Created by bopeng on 2017/12/27.
+ */
+
+public class Constants {
+    public static final String WX_APP_ID = "wx844acb9513c8cbce";
+}
---
+++ app/src/main/java/cn/tthud/taitian/wxapi/WXPayEntryActivity.java
@@ -0,0 +1,61 @@
+package cn.tthud.taitian.wxapi;
+
+import cn.tthud.taitian.utils.Constants;
+
+import com.tencent.mm.opensdk.openapi.IWXAPI;
+import com.tencent.mm.opensdk.openapi.IWXAPIEventHandler;
+import com.tencent.mm.opensdk.openapi.WXAPIFactory;
+import com.tencent.mm.opensdk.constants.ConstantsAPI;
+import com.tencent.mm.opensdk.modelbase.BaseReq;
+import com.tencent.mm.opensdk.modelbase.BaseResp;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.Toast;
+
+public class WXPayEntryActivity extends Activity implements IWXAPIEventHandler{
+	
+
+    private IWXAPI api;
+	
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+    	api = WXAPIFactory.createWXAPI(this, Constants.WX_APP_ID, false);
+		try{
+			api.handleIntent(getIntent(), this);
+		}catch (Exception e){
+			e.printStackTrace();
+		}
+    }
+
+	@Override
+	protected void onNewIntent(Intent intent) {
+		super.onNewIntent(intent);
+		setIntent(intent);
+        api.handleIntent(intent, this);
+	}
+
+	@Override
+	public void onReq(BaseReq req) {
+	}
+
+	@Override
+	public void onResp(BaseResp resp) {
+
+		int result = 0;
+		//有时候支付结果还需要发送给服务器确认支付状态
+		if (resp.getType()== ConstantsAPI.COMMAND_PAY_BY_WX){
+			if (resp.errCode==0){
+				Toast.makeText(this,"支付成功",Toast.LENGTH_LONG).show();
+			}else if (resp.errCode==-2){
+				Toast.makeText(this,"取消支付",Toast.LENGTH_LONG).show();
+			}else {
+				Toast.makeText(this,"支付失败",Toast.LENGTH_LONG).show();
+			}
+			finish();
+		}
+	}
+}
